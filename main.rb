f660145f5bb9d73995d8b76a80dfc8fad208edf3
Isaac Elias <nicecurelom@gmail.com>
2013-06-01 23:40:16 -0700

    Added ...... like everything

---
+++ main.rb
@@ -0,0 +1,52 @@
+require "sinatra"
+require "sinatra/reloader"
+require "movies"
+require "stock_quote"
+require "image_suckr"
+
+get "/" do
+  erb :index
+end
+
+get "/movies" do
+  unless params["mtitle"].nil?
+  	@movie = Movies.find_by_title(params["mtitle"])
+
+  end
+  erb :movies
+
+end
+
+get "/stocks" do
+  unless params["symbol"].nil?
+  	begin
+  		@s = params["symbol"]
+  		@company = StockQuote::Stock.quote(@s.upcase)
+  	rescue
+  		@company = nil 
+  		@s = nil
+  	end
+  end
+
+  erb :stocks
+end
+
+
+get "/images" do
+
+	unless params["query"].nil?
+		begin
+			@q = params["query"]
+			suckr = ImageSuckr::GoogleSuckr.new  
+			@src = suckr.get_image_url({"q" => "#{@q}"})
+		rescue
+			@q = nil
+			@src = nil
+		end
+	end
+
+  erb :images
+end
+
+
+
